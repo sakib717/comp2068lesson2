9d34064fa8bf08ecf1c85219164bd6cfaecb3979
sakib717 <sakibsabbir7@gmail.com>
2016-03-10 23:23:56 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+"use strict";
+var http = require('http');
+var hostname = '127.0.0.1';
+var port = 7171;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port, hostname, function () {
+    console.log("Server running at http://" + hostname + ":" + port);
+});
+//# sourceMappingURL=index.js.map
