d6ea2413ae7bb4410fc329eaaa647926d2b2e5f6
sakib717 <sakibsabbir7@gmail.com>
2016-03-11 00:21:44 -0500

    changed index to server

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end("Hello World\n");
 }).listen(port);
-//# sourceMappingURL=index.js.map
+//# sourceMappingURL=server.js.map
