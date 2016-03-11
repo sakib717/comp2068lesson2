fa070224efbb70ed0938c2137d8cfcc109477b2a
sakib717 <sakibsabbir7@gmail.com>
2016-03-10 23:42:40 -0500

    updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 "use strict";
 var http = require('http');
-var hostname = '127.0.0.1';
-var port = 7171;
+var port = process.env.PORT || 7171;
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
-}).listen(port, hostname, function () {
-    console.log("Server running at http://" + hostname + ":" + port);
-});
+    res.end("Hello World\n");
+}).listen(port);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
 import http = require('http');
-
-var hostname = '127.0.0.1';
-var port = 7171;
+var port: number = process.env.PORT || 7171;
 
 http.createServer(function(req:http.ServerRequest, res:http.ServerResponse){
     res.writeHead(200,{'Content-Type': 'text/plain'});
     res.end("Hello World\n");
-}).listen(port, hostname, function(){
-      console.log("Server running at http://" + hostname + ":" + port);
-});
+}).listen(port);
